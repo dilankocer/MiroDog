1a4ee3d31e09bad393f1325d73c343340b113168
Yana26Rudenko <yana.khottseva@gmail.com>
2021-10-20 17:15:21 -0400

    Second file one more commit

--- src/main_package/SecondClass.java
+++ src/main_package/SecondClass.java
@@ -3,5 +3,6 @@ package main_package;
 public class SecondClass {
     public static void main(String[] args) {
         System.out.println("Hi everyone!!!");
+        System.out.println("One more");
     }
 }
