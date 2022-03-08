bbf737fd644b1af47111c6c33f5d79ee6c6c6d1a
dilankocer <88249108+dilankocer@users.noreply.github.com>
2022-03-08 10:34:50 -0500

    updated and write some new think

--- src/main_package/FirstClass.java
+++ src/main_package/FirstClass.java
@@ -2,6 +2,6 @@ package main_package;
 
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("I love Miro");
+        System.out.println("I love Miro, but I love Jonathan more.");
     }
 }
